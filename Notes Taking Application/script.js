54495ae01e69337bc1dfc19e05f98dde57c9e5f2
vishw060404 <167417853+vishw060404@users.noreply.github.com>
2024-06-19 07:08:20 +0530

    Add files via upload

---
+++ Notes Taking Application/script.js
@@ -0,0 +1,60 @@
+const addBtn =document.querySelector("#addbtn");
+const main = document.querySelector("#main");
+
+
+
+function addNote(){
+    const note = document.createElement("div");
+    note.classList.add("note");
+    note.innerHTML = `
+    <div class="tool">
+        <i class="save fas fa-save"></i>
+        <i class="trash fas fa-trash"></i>
+    </div>
+    <textarea></textarea>
+    `;
+    main.appendChild(note);   
+    const save = note.querySelector(".save");
+    const trash = note.querySelector(".trash");     
+    const textarea = note.querySelector("textarea");  
+
+    save.addEventListener("click",saveNotes);
+    textarea.addEventListener("input",saveNotes);
+    trash.addEventListener("click",()=>{
+        note.remove();
+        saveNotes();
+    });
+    
+    
+
+}
+addBtn.addEventListener("click",addNote);
+
+function saveNotes(){
+    const notes = document.querySelectorAll(".note textarea");
+    const data = Array.from(notes).map(note => note.value);
+    console.log(notes,data)
+
+    if(data.length===0){
+        localStorage.removeItem("notes");
+    }else{
+        localStorage.setItem("notes",JSON.stringify(data));
+    }
+}
+
+function loadNotes(){
+    const Isnotes = JSON.parse(localStorage.getItem("notes"));
+
+    if(Isnotes !==null){
+        Isnotes.forEach( noteText =>{
+            addNote();
+
+            const notes= document.querySelectorAll(".note textarea");
+            const lastNote = notes[notes.length-1];
+            lastNote.value =noteText; 
+        });
+    }else{
+        addNote();
+    }
+}
+loadNotes();
